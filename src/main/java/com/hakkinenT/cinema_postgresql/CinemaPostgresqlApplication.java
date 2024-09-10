420d5bcd3236fe2744e16852cdf71e0a95cd1d85
Tawanna <thol_16@hotmail.com>
2024-09-10 20:53:32 -0300

    Project created

---
+++ src/main/java/com/hakkinenT/cinema_postgresql/CinemaPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.cinema_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakkinenT/cinema_postgresql/CinemaPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.cinema_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
